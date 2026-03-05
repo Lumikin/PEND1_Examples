8dcd54a9bd7c8882d3e9048885982c2f63d88297
Lumikin <lumikomio02@gmail.com>
2026-03-05 09:04:45 -0300

    Feat: add boostrap

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 // getElementById (Capturar elemento pelo Id)
 // console.log(estoqueProduto)
+// estoqueProduto.querySelector (Seleciona por consulta)
 
 let estoqueProduto = document.getElementById("estoqueProduto")
-// estoqueProduto.querySelector (Seleciona por consulta)
-estoqueProduto.innerText = `Estoque: 100 unidades`
 
 let mensagem = document.querySelector("#mensagem")
-mensagem.innerText = `Ola Mundo`
