da2bba1ab5686c28c289ee4863f7c7e24a8dff3a
Lumikin <lumikomio02@gmail.com>
2026-03-05 08:42:50 -0300

    Feat: Alterando arquivos para o contexto da aula

---
+++ script.js
@@ -0,0 +1,9 @@
+// getElementById (Capturar elemento pelo Id)
+// console.log(estoqueProduto)
+
+let estoqueProduto = document.getElementById("estoqueProduto")
+// estoqueProduto.querySelector (Seleciona por consulta)
+estoqueProduto.innerText = `Estoque: 100 unidades`
+
+let mensagem = document.querySelector("#mensagem")
+mensagem.innerText = `Ola Mundo`
