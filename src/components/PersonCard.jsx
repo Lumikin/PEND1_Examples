15fe7e16cd4cdb88628e7f1608b1686419c2d3f6
Lumikin <lucas.m.ribeiro11@aluno.senai.br>
2026-08-27 09:37:23 -0300

    :feat: feat(front): Adicionando Modal no site

--- src/components/PersonCard.jsx
+++ src/components/PersonCard.jsx
@@ -1,6 +1,10 @@
 export default function PersonCard({ person, onClick }) {
   return (
-    <div className="card p-3 mb-3 shadow-sm">
+    <div
+      className="card p-3 mb-3 shadow-sm"
+      onClick={() => onClick(person)}
+      style={{ cursor: "pointer" }}
+    >
       <h5>{person.name}</h5>
       <p>Email: {person.email}</p>
       <p>Telefone: {person.phone}</p>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,16 @@
 import { usePersons } from "../hooks/usePersons";
-import PersonList from "../components/PersonList";
-import SkeletonLoading from "../components/SkeletonLoading";
 import { useContext, useState } from "react";
 import { ThemeContext } from "../contexts/themeContext";
+import SkeletonLoading from "../components/SkeletonLoading";
+
+import PersonList from "../components/PersonList";
+import PersonModal from "../components/PersonModal";
 
 export default function Home() {
   const { theme, togleTheme } = useContext(ThemeContext);
   const { persons, loading } = usePersons();
   const [search, setSearch] = useState("");
+  const [selectedPerson, setSelectedPerson] = useState(null);
   const FilteredPersons = persons.filter(p =>
     p.name.toLowerCase().includes(search.toLowerCase()),
   );
@@ -26,7 +29,20 @@ export default function Home() {
       </button>
       <h1 className="mb-4">Catálogo de Pessoas</h1>
 
-      {loading ? <SkeletonLoading /> : <PersonList persons={FilteredPersons} />}
+      {loading ? (
+        <SkeletonLoading />
+      ) : (
+        <PersonList
+          persons={FilteredPersons}
+          onSelectPerson={setSelectedPerson}
+        />
+      )}
+      {selectedPerson && (
+        <PersonModal
+          person={selectedPerson}
+          onClose={() => selectedPerson(setSelectedPerson(null))}
+        />
+      )}
     </div>
   );
 }
